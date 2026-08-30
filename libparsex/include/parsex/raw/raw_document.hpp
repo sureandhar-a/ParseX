79c299f8312cf531a9620853bc2ab1b6f7e8b85a
sureandhar <sureandharalaguvel@gmail.com>
2026-08-30 11:52:42 +0530

    raw node tree type

--- libparsex/include/parsex/raw/raw_document.hpp
+++ libparsex/include/parsex/raw/raw_document.hpp
@@ -1,5 +1,6 @@
 #include <memory>
 #include <libxml/tree.h>
+#include "raw_node.hpp"
 
 struct XmlDocDeleter {
     void operator()(xmlDoc* doc) const noexcept 
@@ -14,6 +15,7 @@ class RawDocument {
 public:
     explicit RawDocument(XmlDocPtr doc) : doc_(std::move(doc)) {}
     xmlDoc* nativeHandle() const noexcept { return doc_.get(); }
+    RawNode root;
 private:
     XmlDocPtr doc_;
 };
