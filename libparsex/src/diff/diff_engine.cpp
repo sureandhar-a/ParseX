33c0e2f5b0967dd9aff685734a731793078536b6
sureandhar <sureandharalaguvel@gmail.com>
2026-09-22 23:48:58 +0530

    PAR-135: DiffEngine facade with Moved+Modified single-entry handling

---
+++ libparsex/src/diff/diff_engine.cpp
@@ -0,0 +1,14 @@
+#include <parsex/diff/diff_engine.hpp>
+
+#include <parsex/diff/diff_match.hpp>
+#include <parsex/diff/diff_moves.hpp>
+#include <parsex/diff/diff_populate.hpp>
+
+DiffReport DiffEngine::diff(const ParsedProject& oldProject,
+                            const ParsedProject& newProject) const {
+    DiffReport matched = matchParsedProjects(oldProject, newProject);
+    DiffReport moved = applyMoveDetection(oldProject, newProject, std::move(matched));
+    DiffReport populated = populateFieldDiffs(oldProject, newProject, std::move(moved));
+    populated.sortDeterministically();
+    return populated;
+}
